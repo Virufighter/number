f8f3bab40e0e6834e7869c7abe502aca5c1dc6d5
Virufighter <alvinpoulose4@gmail.com>
2022-11-21 12:28:01 +0530

    prime

--- src/Number.java
+++ src/Number.java
@@ -7,5 +7,19 @@ public class Number {
         int result=  c < (a < b ? a : b) ? c : ((a < b) ? a : b);;
         return result;
     }
+    public void prime(int n){
+        int f=1;
+        for (int i=2;i<=n/2;i++){
+            if(n/2==0){
+                f=0;
+                break;
+            }
+        }
+        if(f==1){
+            System.out.printf("prime");
+        }else {
+            System.out.printf("not prime");
+        }
+    }
 
 }
