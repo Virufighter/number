2efa13064390faf589eb829d6bef637fca9f45c8
Virufighter <alvinpoulose4@gmail.com>
2022-11-21 12:54:19 +0530

    main

--- src/Number.java
+++ src/Number.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Number {
     public int largest(int a,int b,int c){
         int result= c > (a > b ? a : b) ? c : ((a > b) ? a : b);
@@ -30,12 +32,51 @@ public class Number {
     }
     public int reverse(int n){
         int r=0;
-        int t=n;
-        while(t>0){
-            r=r+(t%10);
-            t=t/10;
+        while(n>0){
+            r=r*10+(n%10);
+            n=n/10;
         }
         return r;
     }
 
-}
+    public static void main(String[] args) {
+        Number n1=new Number();
+        Scanner sc=new Scanner(System.in);
+        int j=1;
+        while (j==1){
+            System.out.println("1.Largest");
+            System.out.println("2.smallest");
+            System.out.println("3.prime");
+            System.out.println("4.even or not");
+            System.out.println("5.reverse");
+            System.out.println("Enter your choice");
+            int s= sc.nextInt();
+            switch (s){
+                case 1:
+                    System.out.println("enter 3 values");
+                    System.out.println(n1.largest(sc.nextInt(),sc.nextInt(), sc.nextInt()));
+                    break;
+                case 2:
+                    System.out.println("enter 3 values");
+                    System.out.println(n1.smallest(sc.nextInt(),sc.nextInt(), sc.nextInt()));
+                    break;
+                case 3:
+                    System.out.println("enter a value");
+                    n1.prime(sc.nextInt());
+                    break;
+                case 4:
+                    System.out.println("enter a value");
+                    n1.even(sc.nextInt());
+                    break;
+                case 5:
+                    System.out.println("enter a value");
+                    System.out.println(n1.reverse(sc.nextInt()));
+                    break;
+            }
+            System.out.println("do you want to continue 1.yes 2.no");
+            j= sc.nextInt();
+            }
+        }
+    }
+
+
